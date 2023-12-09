3c9ba67a846c58ff3b5070e5f825cfcbaac0ede9
Virtual Educator <152733764+Virtual-Educator@users.noreply.github.com>
2023-12-09 11:49:09 -0800

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,14 @@ import os
 import tempfile
 from io import BytesIO
 
-# Function to combine Word documents
 def combine_word_documents(docs):
     combined_doc = Document()
-
     for doc in docs:
         sub_doc = Document(BytesIO(doc))
         for element in sub_doc.element.body:
             combined_doc.element.body.append(element)
-
     return combined_doc
 
-# Function to process Word files from a ZIP
 def process_zip_file(zip_file):
     with ZipFile(zip_file, 'r') as z:
         with tempfile.TemporaryDirectory() as tempdir:
@@ -40,14 +36,13 @@ def process_zip_file(zip_file):
 
             return word_docs, error_occurred
 
-# Function to process direct Word file uploads
 def process_word_files(word_files):
     return [file.getvalue() for file in word_files]
 
-# Streamlit UI
 st.title('Word Document Combiner')
 
 upload_choice = st.radio("Choose your upload method", ('Zip File', 'Word Files'))
+combined_document = None
 
 if upload_choice == 'Zip File':
     uploaded_file = st.file_uploader("Upload ZIP file", type=['zip'])
@@ -55,23 +50,36 @@ if upload_choice == 'Zip File':
         word_docs, error_occurred = process_zip_file(uploaded_file)
         if word_docs and not error_occurred:
             combined_document = combine_word_documents(word_docs)
-            file_stream = BytesIO()
-            combined_document.save(file_stream)
-            file_stream.seek(0)
-            st.download_button(label="Download Combined Document",
-                               data=file_stream,
-                               file_name="combined_document.docx",
-                               mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document")
 
 elif upload_choice == 'Word Files':
     uploaded_files = st.file_uploader("Upload Word files", accept_multiple_files=True, type=['docx'])
     if st.button('Combine Word Documents') and uploaded_files:
         word_docs = process_word_files(uploaded_files)
         combined_document = combine_word_documents(word_docs)
+
+if combined_document:
+    export_format = st.selectbox("Select export format", ("Word", "PDF", "Text"))
+
+    if st.button('Export Combined Document'):
         file_stream = BytesIO()
         combined_document.save(file_stream)
         file_stream.seek(0)
-        st.download_button(label="Download Combined Document",
-                           data=file_stream,
-                           file_name="combined_document.docx",
-                           mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+
+        if export_format == "Word":
+            st.download_button(label="Download Combined Document",
+                               data=file_stream,
+                               file_name="combined_document.docx",
+                               mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document")
+        elif export_format == "PDF":
+            # PDF conversion logic here (Windows only with docx2pdf)
+            # For cross-platform, use an alternative method
+            pass
+        elif export_format == "Text":
+            text_stream = BytesIO()
+            for paragraph in combined_document.paragraphs:
+                text_stream.write(paragraph.text.encode('utf-8') + b'\n')
+            text_stream.seek(0)
+            st.download_button(label="Download Combined Document",
+                               data=text_stream,
+                               file_name="combined_document.txt",
+                               mime="text/plain")
